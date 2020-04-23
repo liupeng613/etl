6435a984869ecc0c5b3f3556b34b8d511380f954
liupeng613 <garuda613@163.com>
2020-04-23 13:57:23 +0800

    datax3-python3

---
+++ DataX/hdfswriter/src/main/java/com/alibaba/datax/plugin/writer/hdfswriter/SupportHiveDataType.java
@@ -0,0 +1,20 @@
+package com.alibaba.datax.plugin.writer.hdfswriter;
+
+public enum SupportHiveDataType {
+    TINYINT,
+    SMALLINT,
+    INT,
+    INTEGER,
+    BIGINT,
+    FLOAT,
+    DOUBLE,
+    TIMESTAMP,
+    DATE,
+    DECIMAL,
+    STRING,
+    VARCHAR,
+    CHAR,
+    LONG,
+    BOOLEAN,
+    BINARY
+}
